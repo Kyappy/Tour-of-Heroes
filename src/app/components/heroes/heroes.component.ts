07320229ec1873e44490f6526c707a254a582fc3
Kyappy <m.gavanier@gmail.com>
2017-12-17 18:40:48 +0000

    heroService getHeroes method renamed getAll, addHero method renamed post and deleteHero method renamed delete

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HeroService} from '../../services/hero.service';
-import {Hero} from '../../supports/hero';
+import {Hero} from '../../models/hero';
 
 /**
  * App heroes component.
@@ -11,25 +11,30 @@ import {Hero} from '../../supports/hero';
 	templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+	// region public getters
 	/**
 	 * Gets the heroes to display.
 	 * @returns {Hero[]}
 	 */
 	public get heroes(): Hero[] { return this._heroes; }
+	// endregion
 
+	// region private fields
 	/**
 	 * The heroes to display.
 	 * @type {Hero[]}
 	 */
 	private _heroes: Hero[];
+	// endregion
 
 	/**
 	 * Creates a new HeroesComponent instance.
-	 * @param {HeroService} heroService The hero service to inject.
+	 * @param {HeroService} heroService: The hero service to inject.
 	 * @returns {HeroesComponent} A new HeroesComponent instance.
 	 */
 	public constructor(private heroService: HeroService) {}
 
+	// region angular life cycle
 	/**
 	 * Initialize the component.
 	 * @returns {void}
@@ -37,33 +42,36 @@ export class HeroesComponent implements OnInit {
 	public ngOnInit(): void {
 		this.getHeroes();
 	}
+	// endregion
 
 	/**
 	 * Gets the heroes to display.
 	 * @returns {void}
 	 */
 	public getHeroes(): void {
-		this.heroService.getHeroes().subscribe((heroes: Hero[]) => this._heroes = heroes);
+		this.heroService.getAll().subscribe((heroes: Hero[]) => this._heroes = heroes);
 	}
 
+	// region public methods
 	/**
 	 * Adds a new hero.
-	 * @param {string} name The name of the hero to add.
+	 * @param {string} name: The name of the hero to add.
 	 * @returns {void}
 	 */
 	public add(name: string): void {
 		name = name.trim();
 		if (!name) { return; }
-		this.heroService.addHero(new Hero(undefined, name)).subscribe((hero: Hero) => this._heroes.push(hero));
+		this.heroService.post(new Hero(undefined, name)).subscribe((hero: Hero) => this._heroes.push(hero));
 	}
 
 	/**
 	 * Deletes the given hero.
-	 * @param {Hero} hero The hero to delete.
+	 * @param {Hero} hero: The hero to delete.
 	 * @returns {void}
 	 */
 	public remove(hero: Hero): void {
 		this._heroes = this._heroes.filter((h: Hero) => h !== hero);
-		this.heroService.deleteHero(hero).subscribe();
+		this.heroService.delete(hero).subscribe();
 	}
+	// endregion
 }
